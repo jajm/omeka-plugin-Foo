f74f00d99162be3ffffdaff07b435348ab8387a9
Alexis Ben Miloud--Josselin <alexis.benmiloud@biblibre.com>
2017-08-16 17:00:14 +0200

    Initial commit

---
+++ libraries/Foo/Bar.php
@@ -0,0 +1,29 @@
+<?php
+
+use OmekaCli\Application;
+use OmekaCli\Command\AbstractCommand;
+
+class Foo_bar extends AbstractCommand
+{
+    public function getDescription()
+    {
+        return 'print something to stdout';
+    }
+
+    public function getUsage()
+    {
+        $usage = 'Usage:' . PHP_EOL
+               . '    bar' . PHP_EOL
+               . PHP_EOL
+               . 'Print something to stdout.' . PHP_EOL;
+
+        return $usage;
+    }
+
+    public function run($options, $args, Application $application)
+    {
+        echo 'Hello, omeka-cli!' . PHP_EOL;
+
+        return 0;
+    }
+}
